a36daa8d7d2a6ec85fb5fc2cb4089d4a8c356fa6
jlundho <jonas.l.lundholm@gmail.com>
2015-06-19 22:14:01 +0200

    Planning to merge AsyncTaskBranch with Master

--- app/src/main/java/com/jlundhoo/cloudstreamer/MainActivityFragment.java
+++ app/src/main/java/com/jlundhoo/cloudstreamer/MainActivityFragment.java
@@ -1,5 +1,8 @@
 package com.jlundhoo.cloudstreamer;
 
+
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -15,17 +18,11 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Executors;
 
 import kaaes.spotify.webapi.android.SpotifyApi;
 import kaaes.spotify.webapi.android.SpotifyService;
-import kaaes.spotify.webapi.android.models.Album;
 import kaaes.spotify.webapi.android.models.Artist;
 import kaaes.spotify.webapi.android.models.ArtistsPager;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.android.MainThreadExecutor;
-import retrofit.client.Response;
 
 
 /**
@@ -38,13 +35,11 @@ public class MainActivityFragment extends Fragment {
     private ListView artistListView;
     private ImageView searchImageView;
 
-    private Album newAlbum;
-
     private List<Artist> artistSearchResult = new ArrayList<Artist>();
-
 
     private MyAdapter mAdapter; //Create own adapter for additional control
 
+    static final String ARTIST_TAG = "artist";
 
     public MainActivityFragment() {
 
@@ -73,37 +68,55 @@ public class MainActivityFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 String searchString = editTextSearch.getText().toString();
-                searchForArtist(searchString);
+
+                SearchClass mSearchClass = new SearchClass();
+                mSearchClass.execute(searchString);
             }
         });
 
         artistListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast myToast = Toast.makeText(getActivity(), mAdapter.getItemById(position).name, Toast.LENGTH_SHORT);
-                myToast.show();
+                Artist selectedArtist = (Artist) mAdapter.getItem(position);
+
+                Toast toast = Toast.makeText(getActivity(), selectedArtist.name, Toast.LENGTH_SHORT);
+                toast.show();
+
+                Intent intent = new Intent(getActivity(), AlbumActivity.class);
+                intent.putExtra(ARTIST_TAG, "Hej"/*selectedArtist.name*/);
+                startActivity(intent);
             }
         });
         return rootView;
     }
 
-    private void searchForArtist(String searchString){
-
-        SpotifyApi api = new SpotifyApi(Executors.newSingleThreadExecutor(),
-                new MainThreadExecutor());
+    public class SearchClass extends AsyncTask<String, Void, ArtistsPager>{
+        SpotifyApi api = new SpotifyApi();
         SpotifyService spotify = api.getService();
-        spotify.searchArtists(searchString, new Callback<ArtistsPager>() {
-            @Override
-            public void success(ArtistsPager artistsPager, Response response) {
-                artistSearchResult = artistsPager.artists.items;
-                mAdapter.addArtists(artistSearchResult);
+        ArtistsPager myArtistPager;
+
+        @Override
+        protected ArtistsPager doInBackground(String...params) {
+            String searchString = params[0];
+            return spotify.searchArtists(searchString);
+        }
+
+        @Override
+        protected void onPostExecute(ArtistsPager artistPager) {
+            super.onPostExecute(artistPager);
+
+            myArtistPager = artistPager;
+            artistSearchResult = artistPager.artists.items;
+
+            Log.i("Artist", String.valueOf(artistSearchResult.size()));
+            if (artistSearchResult.size() > 0) {
+                for (int i = 0; i < artistSearchResult.size(); i++) {
+                    mAdapter.add(artistSearchResult.get(i));
+                }
+                mAdapter.notifyDataSetChanged();
             }
 
-            @Override
-            public void failure(RetrofitError error) {
-                Log.d(getString(R.string.LOG_TAG), error.toString());
-            }
-        });
+        }
 
     }
 
